e8679a35429e48bf6f6209c00e4490763044fc1c
cianid <cianidtop@icloud.com>
2019-11-12 12:23:40 +0300

    added opportunity to make posts using admin panel

--- home/models.py
+++ home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    date = models.DateTimeField(default=timezone.now)
+    image = models.ImageField()
+
+
+
